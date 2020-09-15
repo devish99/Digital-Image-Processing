c77de3ae44e3ebee56a9777b5cecc6c5b131da56
Vishnu <54411695+devish99@users.noreply.github.com>
2020-09-15 09:12:20 +0530

    Add files via upload

---
+++ BMP_Header.py
@@ -0,0 +1,34 @@
+import struct
+import cv2
+import numpy as np
+
+#Fetching image file from folder
+myfile = 'C:\\Users\\psvis\\OneDrive\\Documents\\Sem 7\\Digital Image Processing Lab\\Wolf.bmp'
+data = cv2.imread(myfile)
+myimage = np.unique(data) #Collect unique elements of array to reduce size of image 
+
+#Reading BMP Header
+bmp = open(myfile, 'rb')
+
+#Printing the Header
+print('Type:', bmp.read(2).decode())
+print('Size: %s' % struct.unpack('I', bmp.read(4)))
+print('Reserved 1: %s' % struct.unpack('H', bmp.read(2)))
+print('Reserved 2: %s' % struct.unpack('H', bmp.read(2)))
+print('Offset: %s' % struct.unpack('I', bmp.read(4)))
+
+print('DIB Header Size: %s' % struct.unpack('I', bmp.read(4)))
+print('Width: %s' % struct.unpack('I', bmp.read(4)))
+print('Height: %s' % struct.unpack('I', bmp.read(4)))
+print('Colour Planes: %s' % struct.unpack('H', bmp.read(2)))
+print('Bits per Pixel: %s' % struct.unpack('H', bmp.read(2)))
+print('Compression Method: %s' % struct.unpack('I', bmp.read(4)))
+print('Raw Image Size: %s' % struct.unpack('I', bmp.read(4)))
+print('Horizontal Resolution: %s' % struct.unpack('I', bmp.read(4)))
+print('Vertical Resolution: %s' % struct.unpack('I', bmp.read(4)))
+print('Number of Colours: %s' % struct.unpack('I', bmp.read(4)))
+print('Important Colours: %s' % struct.unpack('I', bmp.read(4)))
+
+#Printing the image as an array
+arr = np.asarray(data)
+print (arr)
